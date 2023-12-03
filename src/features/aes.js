34ae01570ce1ac63ccbdf3853abf971b3cf38b33
ahmedelsayed968 <ahmedeltop968@gmail.com>
2023-12-04 01:44:31 +0200

    feat: extract key length from the given algorithm and validate this algorithm

--- src/features/aes.js
+++ src/features/aes.js
@@ -1,5 +1,5 @@
-import { scrypt, randomFill, createCipheriv } from 'node:crypto-js';
-import {createReadStream,createWriteStream} from 'node:fs';
+const { scrypt, randomFill, createCipheriv } = require('node:crypto');
+const {createReadStream,createWriteStream} = require('node:fs');
 
 class AES{
     /**
@@ -19,6 +19,8 @@ class AES{
         this.password = password;
         this.file = inFile;
         this.outputFile = outFile
+        this.algorithmKeyLengths = [128,192,256]
+        this.isValidAlgorithm = this.validateAlgorithm()
         this.keyLength = this.getKeyLength()
     }
     encrypt (){
@@ -28,9 +30,28 @@ class AES{
     decrypt(){
         // do the decryption on the passed file
     }
+    /**
+     *  aes-128-cbc: Uses a 128-bit key (16 bytes)
+        aes-128-ecb: Also uses a 128-bit key (16 bytes)
+        aes-192-cbc: Uses a 192-bit key (24 bytes)
+        aes-192-ecb: Also uses a 192-bit key (24 bytes)
+        aes-256-cbc: Uses a 256-bit key (32 bytes)
+        aes-256-ecb: Also uses a 256-bit key (32 bytes)
+     *
+     * @return {*} 
+     * @memberof AES
+     */
     getKeyLength(){
         // helper function to get the length of the key from the given algorithm 
+        const [,num,] = this.algorithm.toLowerCase().split('-');
+        return parseInt(num/8,10)
+    }
+    validateAlgorithm(){
+        const [aes,num,type] = this.algorithm.toLowerCase().split('-');
+        if (aes !== 'aes' || (type !== 'cbc' && type !=='ecb') || !this.algorithmKeyLengths.includes(parseInt(num,10)))
+            return false
+        return true
     }
 }
 
-module.exports = AES;
+module.exports = AES
---
+++ tests/aes.test.js
@@ -0,0 +1,32 @@
+const AES =  require('../src/features/aes');
+
+describe('test key length',()=>{
+    test('Test #1' , ()=>{
+        const p = new AES('aes-128-ecb',"","","")
+        expect(p.keyLength).toBe(16);
+    })
+    test('Test #2' , ()=>{
+        const p = new AES('aes-256-ecb',"","","");
+        expect(p.keyLength).toBe(32);
+    })
+
+})
+
+describe('test validateAlgorithm', ()=>{
+    test('Test #1',()=>{
+        const p = new AES('aes-128-ecb',"","","")
+        expect(p.isValidAlgorithm).toBe(true);
+    })
+    test('Test #2',()=>{
+        const p = new AES('aes-256-ecb',"","","")
+        expect(p.isValidAlgorithm).toBe(true);
+    })
+    test('Test #3',()=>{
+        const p = new AES('ase-128-ecb',"","","")
+        expect(p.isValidAlgorithm).toBe(false);
+    })
+    test('Test #4',()=>{
+        const p = new AES('aes-128-ecbd',"","","")
+        expect(p.isValidAlgorithm).toBe(false);
+    })
+})
