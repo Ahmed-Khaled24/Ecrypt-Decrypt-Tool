6898bfd3665911e2a06b3eb4a26a77d13e24af17
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-14 13:41:30 +0200

    refactor: add jsdoc to generateHash function

--- src/features/sha512.js
+++ src/features/sha512.js
@@ -1,5 +1,9 @@
 const { createHash } = require("node:crypto");
 
+/**
+ * @param {string} data the data to generate hash for.
+ * @returns
+ */
 function generateHash(data) {
     const hash = createHash("sha512");
     hash.update(data);
