9f763296813d020a1d4db4b7fa26b0308270c459
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-17 16:08:57 +0200

    refactor: change how aes handle file names

--- src/features/encrypt-sign_decrypt-verify.js
+++ src/features/encrypt-sign_decrypt-verify.js
@@ -2,17 +2,16 @@ const { KeyObject } = require("crypto");
 const { readFileSync } = require("fs");
 const { readFile, writeFile } = require("fs/promises");
 const { generateHash } = require("./sha512");
-const { join } = require("path");
 const { sign, verify } = require("./sign-verify");
 const AES = require("./aes");
 
 /**
- * @param {KeyObject | string} privateKey rsa private key
+ * @param {string} privateKeyPath rsa private key absolute file path
  * @param {string} symmetricKey key to the aes algorithm
  * @param {string} sourcePath absolute path to the file that you want to encrypt and sign
- * @param {string} targetPath absolute path to the file that will be generated with the result of decryption and singing
  */
-async function encrypt_sign(privateKey, symmetricKey, sourcePath, targetPath) {
+async function encrypt_sign(privateKeyPath, symmetricKey, sourcePath) {
+    const privateKey = readFileSync(privateKeyPath);
     return new Promise(async (resolve, reject) => {
         const fileData = await readFile(sourcePath, "binary");
         const fileHash = generateHash(fileData);
@@ -29,12 +28,7 @@ async function encrypt_sign(privateKey, symmetricKey, sourcePath, targetPath) {
             encoding: "binary",
         });
 
-        const aes = new AES(
-            "aes-256-ecb",
-            symmetricKey,
-            sourcePath,
-            targetPath,
-        );
+        const aes = new AES("aes-256-ecb", symmetricKey, sourcePath);
 
         aes.encrypt(async (resultBoolean) => {
             if (resultBoolean) {
@@ -49,19 +43,14 @@ async function encrypt_sign(privateKey, symmetricKey, sourcePath, targetPath) {
 }
 
 /**
- * @param {KeyObject | string} publicKey rsa public key
+ * @param {string} publicKeyPath rsa private key absolute file path
  * @param {string} symmetricKey key to the aes algorithm
  * @param {string} sourcePath absolute path to the file that you want to decrypt and verify
- * @param {string} targetPath absolute path to the file that will be generated with the result of decryption
  */
-async function decrypt_verify(publicKey, symmetricKey, sourcePath, targetPath) {
+async function decrypt_verify(publicKeyPath, symmetricKey, sourcePath) {
+    const publicKey = readFileSync(publicKeyPath);
     return new Promise((resolve, reject) => {
-        const aes = new AES(
-            "aes-256-ecb",
-            symmetricKey,
-            sourcePath,
-            targetPath,
-        );
+        const aes = new AES("aes-256-ecb", symmetricKey, sourcePath);
         aes.decrypt(async (resultBoolean) => {
             console.log(
                 resultBoolean
@@ -69,6 +58,7 @@ async function decrypt_verify(publicKey, symmetricKey, sourcePath, targetPath) {
                     : reject("The code has been exploded"),
             );
 
+            const targetPath = sourcePath.replace(".txt", ".dec.txt");
             const fileData = await readFile(targetPath, "binary");
             let [actualData, signature] = fileData.split("\n#SIGNATURE#");
 
